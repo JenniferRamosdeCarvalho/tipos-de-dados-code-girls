9a8bc4ef291f2a081769e408efa6bbe537d8f4f0
Perkles <jenniferramosdecarvalho@gmail.com>
2022-07-13 15:49:06 -0300

    tipos de dados - treinando

---
+++ src/tiposdedados/exercicio2/exercicio2/Main.java
@@ -0,0 +1,66 @@
+package tiposdedados.exercicio2.exercicio2;
+
+public class Main {
+    public static void main(String[] args) {
+
+        byte b1 = 15;
+        byte b2 = 20;
+
+        short s1 = 1000;
+        short s2 = 3000;
+
+        ///int i1 = -187638;
+        int i2 = 30005;
+
+        long l1 = 56377287989782882L;
+        long l2 = 98789378767372728L;
+
+        float f1 = 4.5f;
+        float f2 = 7.3f;
+
+        double d1 = 75.5d;
+        double d2 = 87.3d;
+
+        char c1 = 'M';
+        char c2 = 'T';
+        char c3 = '\u0057';
+
+        String st1 = "Fulano";
+        String st2 = "Ciclano";
+
+        boolean bo1 = true;
+        boolean bo2 = false;
+
+        System.out.println(b1);
+        System.out.println(b2);
+
+        System.out.println(s1);
+        System.out.println(s2);
+
+        System.out.println(l1);
+        System.out.println(l2);
+
+        System.out.println(f1);
+        System.out.println(f2);
+
+        System.out.println(d1);
+        System.out.println(d2);
+
+        System.out.println(c1);
+        System.out.println(c2);
+
+        System.out.println(i2);
+        System.out.println(c3);
+
+        System.out.println(st1);
+        System.out.println(st2);
+
+        System.out.println(bo1);
+        System.out.println(bo2);
+
+
+
+
+    }
+
+}
